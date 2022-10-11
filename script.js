79f041f3cbdbd0146dc58d16a22c0dcc004ccca3
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-10-11 16:35:50 +0630

    Setup eslint

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// To add menu for interactive 
+const openMenu = document.getElementById('openMenu');
+const closeMenu = document.getElementById('closeMenu');
+const overlay = document.getElementById('overlay');
+const upper = document.querySelector('.upper');
 
-let openMenu = document.getElementById('openMenu')
-let closeMenu = document.getElementById('closeMenu')
-let upper = document.querySelector('.upper')
-
-openMenu.onclick = ()=>{
-    open()
-}
-closeMenu.onclick = ()=>{
-    close()
-}
-
-function close(){
-    openMenu.style.display = "block";
-    overlay.style.display = "none";
-    document.body.style.position = "relative";
-    upper.classList.remove('blur')  
+function close() {
+  openMenu.style.display = 'block';
+  overlay.style.display = 'none';
+  document.body.style.position = 'relative';
+  upper.classList.remove('blur');
 }
 
-function open(){
-    openMenu.style.display = "none";
-    overlay.style.display = "block";
-    document.body.style.position = "fixed";
-    upper.classList.add('blur')  
+function open() {
+  openMenu.style.display = 'none';
+  overlay.style.display = 'block';
+  document.body.style.position = 'fixed';
+  upper.classList.add('blur');
 }
 
+openMenu.onclick = () => {
+  open();
+};
+
+closeMenu.onclick = () => {
+  close();
+};
